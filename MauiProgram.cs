e4ee4f2d47840c27588a471d3129d3018ce13b9c
Richard <rchrdperreault@gmail.com>
2026-07-17 07:13:07 -0400

    Added var to MauiProgram.cs

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -7,8 +7,9 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
-		builder
-			.UseMauiApp<App>()
+        var _ = typeof(NotificationsBridgeClean.Platforms.Android.NotificationListener);
+        builder
+            .UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
